c5a17434fb53696dc51c1ffbee3b40ed58a92555
ahmed kadry <ahmedkadry123a@gmail.com>
2023-05-21 02:57:26 +0200

    edit gitignore

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '864861651152-csp4mc16nf95jdiisf3e1olhjn28rqlk.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI:'mongodb+srv://ahmedkadry123a:ahmedtay1234A@cluster0.9uobydc.mongodb.net/?retryWrites=true&w=majority',
+    cookiekey: 'htrdytrdyrdr'
+};
